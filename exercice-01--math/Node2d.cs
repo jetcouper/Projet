07e468c27a18ba94bec5e773f9863a6c58aec489
jetcouper <ant7945@gmail.com>
2026-02-06 15:20:04 -0500

    PacMan fin et teste avec manette réussi

--- exercice-01--math/Node2d.cs
+++ exercice-01--math/Node2d.cs
@@ -3,12 +3,28 @@ using Godot;
 
 public partial class Node2d : Node2D
 {
-    private Vector2 posBiscuit = new Vector2(0 + 50, 0 + 50);
-    private bool boucheDoitOuvrir = true;
-    private float bouche = Mathf.Pi / 4f;
+    private Vector2 posBiscuit = new Vector2(50, 50);
+    private Vector2 posPac = new(400, 100);
+    private Vector2 currentInputVector = new(0, 0);
+
+    [ExportGroup("Biscuit")]
+    private float vitesseMouvement = 100.0f;
+
+    [ExportGroup("PacMan")]
+    [Export]
+    private float frequenceFermeture = 2.0f;
+
+    [Export]
+    private float angleRadPacManAmplitude = Mathf.Pi * 0.1f;
+
+    [Export]
+    private float angleRadPacManAmplitudeDecalage = Mathf.Pi * 0.1f;
 
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready() { }
+    public override void _Ready()
+    {
+        GD.Print("Salut tata!");
+    }
 
     public override void _Draw()
     {
@@ -26,21 +42,30 @@ public partial class Node2d : Node2D
 
         DrawCircle(posBiscuit, 100.0f, Colors.Chocolate, true, -1, false);
         DrawRect(
-            new Rect2(new Vector2(posBiscuit.X + 20, 0 + 20), new Vector2(0 + 20, 0 + 20)),
+            new Rect2(
+                new Vector2(posBiscuit.X + 80, posBiscuit.Y - 10),
+                new Vector2(0 + 20, 0 + 20)
+            ),
             Colors.Brown,
             true,
             -1,
             false
         );
         DrawRect(
-            new Rect2(new Vector2(posBiscuit.X + 70, 0 + 70), new Vector2(0 + 20, 0 + 20)),
+            new Rect2(
+                new Vector2(posBiscuit.X + 50, posBiscuit.Y - 5),
+                new Vector2(0 + 20, 0 + 20)
+            ),
             Colors.Brown,
             true,
             -1,
             false
         );
         DrawRect(
-            new Rect2(new Vector2(posBiscuit.X + 20, 0 + 65), new Vector2(0 + 20, 0 + 20)),
+            new Rect2(
+                new Vector2(posBiscuit.X - 50, posBiscuit.Y - 80),
+                new Vector2(0 + 20, 0 + 20)
+            ),
             Colors.Brown,
             true,
             -1,
@@ -58,29 +83,29 @@ public partial class Node2d : Node2D
         // DrawCircle(posPepitteC, 10, Colors.Black, true);
 
         //Pacman
-        Vector2 posPac = new(400, 100);
+        //Vector2 posPac = new(400, 100);
 
-        if (boucheDoitOuvrir)
-        {
-            bouche += 0.05f;
-            if (bouche >= Mathf.Pi / 2f)
-            {
-                boucheDoitOuvrir = false;
-            }
-        }
-        else
-        {
-            bouche -= 0.05f;
-            if (bouche <= 0f)
-            {
-                boucheDoitOuvrir = true;
-            }
-        }
-        float debutAngle = bouche / 2f;
-        float finAngle = 2 * Mathf.Pi - debutAngle;
+        float tempsTotalMl = Time.GetTicksMsec();
+        float tempsTotalSec = tempsTotalMl / 1000.0f;
 
-        DrawArc(posPac, 50, debutAngle, finAngle, 60, Colors.Yellow, 100.0f);
-        DrawCircle(posPac + new Vector2(15, -30), 5, Colors.Black);
+        //Truc: diviser l'angle du sinus par 2 PI multiplier par une fréquence pour
+        // pouvoir décider combien de cycles par secondes
+        float valeurSinPacManAnimation = Mathf.Sin(
+            frequenceFermeture * tempsTotalSec * (2.0f * Mathf.Pi)
+        );
+        float angleOuvertureAnimation =
+            angleRadPacManAmplitudeDecalage + angleRadPacManAmplitude * valeurSinPacManAnimation;
+
+        DrawArc(
+            posPac,
+            50,
+            angleOuvertureAnimation,
+            2.0f * Mathf.Pi - angleOuvertureAnimation,
+            60,
+            Colors.Yellow,
+            100.0f
+        );
+        DrawCircle(posPac + new Vector2(-5, -30), 10, Colors.Black);
     }
 
     public override void _Process(double delta)
@@ -88,11 +113,16 @@ public partial class Node2d : Node2D
         base._Process(delta);
         //Rapel draw
         QueueRedraw();
+        //Lecture des touches dans process (correspond à quand l'écran s'affiche)
+        currentInputVector = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
     }
 
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
-        posBiscuit.X += 1;
+        //On multiplie toujours les vitesses par delta qui est la durée depuis la
+        // dernier appel de fonction
+        //posBiscuit += currentInputVector * vitesseMouvement * (float)delta;
+        posPac += currentInputVector * vitesseMouvement * (float)delta;
     }
 }
