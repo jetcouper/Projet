4fc1aac21b260b459741c2e4f9f4b3bbd0c8f1f6
jetcouper <ant7945@gmail.com>
2026-02-27 15:10:13 -0500

    asdf

--- 02---forest-dash/Chien.cs
+++ 02---forest-dash/Chien.cs
@@ -15,4 +15,15 @@ public partial class Chien : Sprite2D
     [ExportGroup("Internal")]
     [Export]
     Poursuivant poursuite;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        Scale = Vector2.Zero;
+        Tween tween = CreateTween();
+        tween
+            .TweenProperty(this, "scale", Vector2.One, 0.5f)
+            .SetTrans(Tween.TransitionType.Back)
+            .SetEase(Tween.EaseType.Out);
+    }
 }
